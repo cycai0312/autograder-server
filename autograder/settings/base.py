e9b6c871e0a25b7173afca79591fe2ae614587f0
James Perretta <jameslp@umich.edu>
2024-08-26 09:19:29 -0400

    Version 2024.08.0. First use of calendar versioning.

--- autograder/settings/base.py
+++ autograder/settings/base.py
@@ -7,7 +7,7 @@ import sys
 
 from django.utils.crypto import get_random_string
 
-VERSION = '4.5.0'
+VERSION = '2024.08.0'
 
 # This is the autograder-server directory
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
